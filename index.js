f3a22cdc35fc19d6cc68bd159b92d1d6ed657b36
Ágnes Kuti-Palasthy <agnesnora@gmail.com>
2022-10-09 17:26:20 +0200

    formData()

--- index.js
+++ index.js
@@ -11,6 +11,12 @@ closeBtn.addEventListener("click", function () {
 const consentForm = document.getElementById("consent-form");
 consentForm.addEventListener("submit", function (e) {
   e.preventDefault();
+
+  const consentFormData = new FormData(consentForm);
+  console.log(consentFormData);
+
+  const name = consentFormData.get("name");
+
   modalText.innerHTML = `<img src="images/loading.svg" class="loading">
   <p id="uploadText">
       Uploading your data to the dark web...
@@ -24,7 +30,7 @@ consentForm.addEventListener("submit", function (e) {
     () =>
       (document.getElementById(
         "modal-inner"
-      ).innerHTML = `<h2>Thanks you sucker! </h2>
+      ).innerHTML = `<h2>Thanks <span class="modal-display-name">${name}</span> you sucker! </h2>
   <p>We just sold the rights to your eternal soul.</p>
   <div class="idiot-gif">
       <img src="images/pirate.gif">
