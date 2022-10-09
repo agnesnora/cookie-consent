84b3b929141cf2ad8add54f4ad85d752ff5f9ac1
Ágnes Kuti-Palasthy <agnesnora@gmail.com>
2022-10-09 14:39:07 +0200

    modal messages JS

--- index.js
+++ index.js
@@ -16,29 +16,19 @@ consentForm.addEventListener("submit", function (e) {
       Uploading your data to the dark web...
   </p>
 </div>`;
-  const uploadText = document.getElementById("uploadText");
   setTimeout(
-    () =>
-      (uploadText.innerHTML = `
-<p id="uploadText">
-    Making the sale
-</p>
-</div>`),
+    () => (document.getElementById("uploadText").innerText = `Making the sale`),
     1500
   );
+  setTimeout(
+    () =>
+      (document.getElementById(
+        "modal-inner"
+      ).innerHTML = `<h2>Thanks you sucker! </h2>
+  <p>We just sold the rights to your eternal soul.</p>
+  <div class="idiot-gif">
+      <img src="images/pirate.gif">
+  </div>`),
+    3500
+  );
 });
-
-/*
-Challenge:
-1. Take control of the "modal-text" element. 
-2. Make it so that when a user clicks on 
-   the accept button, the HTML string below
-   is inserted into the modal-text div.
-
-<div class="modal-inner-loading">
-    <img src="images/loading.svg" class="loading">
-    <p id="uploadText">
-        Uploading your data to the dark web...
-    </p>
-</div>
-*/
